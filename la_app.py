d3cc8db5eca148bb19f80c9edcf31faedbbe538d
tefaweb000-creator <tefaweb000@gmail.com>
2026-08-20 07:18:06 -0500

    Fix image filename typo in la_app.py

--- la_app.py
+++ la_app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title("Tengo más trabajos que vida")
 st.header ("ya no aguanto más")
 st.write("y tengo hambre")
-image= Image.open("perrollora.png")
+image= Image.open("perrolllora.png")
 st.Image(image,caption= "interfaces multimodales")
