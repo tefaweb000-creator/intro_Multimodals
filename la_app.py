28fadd1c8e6a26e74756f994d515ee6a71dfb393
tefaweb000-creator <tefaweb000@gmail.com>
2026-08-20 07:29:45 -0500

    Update la_app.py

--- la_app.py
+++ la_app.py
@@ -6,3 +6,21 @@ st.header ("ya no aguanto más")
 st.write("y tengo hambre")
 image= Image.open("perrolllora.png")
 st.image(image,caption= "interfaces multimodales")
+
+texto= st.text_input ("¿qué puedo tragar?)
+st.write("el texto escrito es", texto)
+st.subheader ("ahora usamos dos columnas")
+col1,col2 = st(columns(2)
+
+with col1;
+    st.subheader ("primera")
+    st.write("el texto escrito es", texto)
+    resp = st.checkbox ("estoy de acuerdo")
+    if resp; st.write ("correcto")
+
+with col2:
+  st.subheader ("y la otra")
+modo= st.radio ("qué modalidad es esto", ("visual", "auditiva", "táctil"))
+
+                      
+      
